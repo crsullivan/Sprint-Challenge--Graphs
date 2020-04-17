0684c70ade62aef32c231c90723cd757cfd06a12
Connor Sullivan <crsullivan@protonmail.com>
2020-04-17 10:20:47 -0700

    mvp

--- adv.py
+++ adv.py
@@ -5,6 +5,8 @@ from world import World
 import random
 from ast import literal_eval
 
+from util import Stack, Queue
+
 # Load world
 world = World()
 
@@ -28,10 +30,78 @@ player = Player(world.starting_room)
 # Fill this out with directions to walk
 # traversal_path = ['n', 'n']
 traversal_path = []
+visited = {}
+visited[player.current_room] = player.current_room.get_exits()
+print('starting room exits:', visited.get(player.current_room))
+print("Nearby:", room_graph[0][1])
+
+def bfs(starting_vertex, destination_vertex):
+    qq = Queue()
+    qq.enqueue([starting_vertex])
+    visit = set()
+    while qq.size() > 0:
+        path = qq.dequeue()
+        last_vertex = path[-1]
+        if last_vertex == destination_vertex:
+                return path
+        if last_vertex not in visit:
+            visit.add(last_vertex)
+            exits = room_graph[last_vertex][1]
+            for direction in exits:
+                qq.enqueue([*path, exits[direction]])
+
+def dft(v):
+    visit = []
+    s = Stack()
+    s.push(v.id)
+    while s.size() > 0:
+        v = s.pop()
+        if v not in visit:
+            visit.append(v)
+            exits = room_graph[v][1]
+            for direction in exits:
+                if exits[direction] not in visit:
+                    s.push(exits[direction])
+    return visit
+
+def dothething(player):
+    path = dft(player.current_room)
+    for i in range(len(path) - 1):
+        location = player.current_room
+        exits = location.get_exits()
+        bfs_path = bfs(path[i], path[i+1])
+        for i in range(len(bfs_path) - 1):
+            room_exits = room_graph[bfs_path[i]][1]
+            for direction in room_exits:
+                if room_exits[direction] == bfs_path[i + 1]:
+                    player.travel(direction)
+                    traversal_path.append(direction)
 
 
+# visit = []
+# s = Stack()
+# print('room:', player.current_room.id)
+# s.push(player.current_room.id)
+# while s.size() > 0:
+#     v = s.pop()
+#     if v not in visit:
+#         print("V:", v)
+#         visit.append(v)
+#         exits = room_graph[v][1]
+#         print(exits)
+#         traversal_path.append(v)
+#         if v not in visited_rooms:
+#             visited_rooms.add(v)
+#         print(visited_rooms, len(visited_rooms))
+#         for direction in exits:
+#             if exits[direction] not in visit:
+#                 s.push(exits[direction])
+# print('DFT Return:', visit)
+# print(len(visited_rooms))
+# print(len(room_graph))
 
-# TRAVERSAL TEST
+# TRAVERSAL TEST`
+dothething(player)
 visited_rooms = set()
 player.current_room = world.starting_room
 visited_rooms.add(player.current_room)
@@ -46,17 +116,17 @@ else:
     print("TESTS FAILED: INCOMPLETE TRAVERSAL")
     print(f"{len(room_graph) - len(visited_rooms)} unvisited rooms")
 
-
-
+print("Traversal Length:", len(traversal_path))
+print("Follow These Directions:", traversal_path)
 #######
 # UNCOMMENT TO WALK AROUND
 #######
-player.current_room.print_room_description(player)
-while True:
-    cmds = input("-> ").lower().split(" ")
-    if cmds[0] in ["n", "s", "e", "w"]:
-        player.travel(cmds[0], True)
-    elif cmds[0] == "q":
-        break
-    else:
-        print("I did not understand that command.")
+# player.current_room.print_room_description(player)
+# while True:
+#     cmds = input("-> ").lower().split(" ")
+#     if cmds[0] in ["n", "s", "e", "w"]:
+#         player.travel(cmds[0], True)
+#     elif cmds[0] == "q":
+#         break
+#     else:
+#         print("I did not understand that command.")
